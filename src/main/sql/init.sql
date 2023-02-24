fdfff09afcbc33f517a38520fe916864d2c697c2
enenen010 <enenen010@naver.com>
2023-02-24 16:13:37 +0900

    ddd

--- src/main/sql/init.sql
+++ src/main/sql/init.sql
@@ -14,3 +14,10 @@ INSERT INTO dept VALUES (20,'RESEARCH','DALLAS');
 INSERT INTO dept VALUES (30,'SALES','CHICAGO');
 INSERT INTO dept VALUES (40,'OPERATIONS','BOSTON');
 INSERT INTO dept VALUES (50,'블루','부산');
+
+
+create user 'scott'@'locahost' identified by 'tiger';
+create user 'scott'@'%' identified by 'tiger';
+GRANT ALL privileges ON *.* TO 'scott'@'locahost';
+GRANT ALL privileges ON *.* TO 'scott'@'%';
+flush privileges;
